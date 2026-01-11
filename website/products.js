32fb054fd4e994a50792774b4887d6f8fd7671ec
Bumble-And-Yarn <patrickpirlea@gmail.com>
2026-01-11 16:25:44 +0000

    Update products.js

--- website/products.js
+++ website/products.js
@@ -4,37 +4,49 @@ const products = [
     name: "Flower Petal Coaster",
     image: "flower petal coaster.jpg",
     category: "coasters",
-    price: 7.20
+    options: [
+      { label: "Pink", price: 7.20 },
+      { label: "Cream", price: 7.20 }
+    ]
   },
   {
     id: 2,
     name: "Flurry Coaster",
     image: "flurry coaster.jpg",
     category: "coasters",
-    price: 7.20
+    options: [
+      { label: "White", price: 7.20 },
+      { label: "Blue", price: 7.20 }
+    ]
   },
   {
     id: 3,
     name: "Helm Coaster",
     image: "helm coaster.jpg",
     category: "coasters",
-    price: 7.20
+    options: [
+      { label: "Natural", price: 7.20 },
+      { label: "Grey", price: 7.20 }
+    ]
   },
   {
     id: 4,
-    name: "Cactus",
+    name: "Cactus Coaster",
     image: "cactus coaster.jpg",
-    category: "cactus",
-    price: 8.00
+    category: "coasters",
+    options: [
+      { label: "Green", price: 8.00 },
+      { label: "Dark Green", price: 8.00 }
+    ]
   }
 ];
 
 const container = document.getElementById("products");
 const filter = document.getElementById("categoryFilter");
 
-function addToBasket(product) {
+function addToBasket(item) {
   const basket = JSON.parse(localStorage.getItem("basket")) || [];
-  basket.push(product);
+  basket.push(item);
   localStorage.setItem("basket", JSON.stringify(basket));
   alert("Added to basket");
 }
@@ -47,13 +59,30 @@ function renderProducts(category = "all") {
     .forEach(p => {
       const div = document.createElement("div");
       div.className = "product";
+
+      const optionsHtml = p.options
+        .map(
+          (o, i) =>
+            `<option value="${i}">${o.label} - £${o.price.toFixed(2)}</option>`
+        )
+        .join("");
+
       div.innerHTML = `
         <img src="${p.image}">
         <h3>${p.name}</h3>
-        <p>£${p.price.toFixed(2)}</p>
+        <select class="option-select">${optionsHtml}</select>
         <button>Add to basket</button>
       `;
-      div.querySelector("button").onclick = () => addToBasket(p);
+
+      div.querySelector("button").onclick = () => {
+        const select = div.querySelector(".option-select");
+        const opt = p.options[select.value];
+        addToBasket({
+          name: `${p.name} (${opt.label})`,
+          price: opt.price
+        });
+      };
+
       container.appendChild(div);
     });
 }
